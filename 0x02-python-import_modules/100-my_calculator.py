375d11b819b1b9c70eb98c21a7c5c34f6daa9110
Rida <redaessahi@gmail.com>
2024-01-04 14:45:10 +0100

    My Calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+from sys import argv
 
 if __name__ == "__main__":
-    """Handle basic arithmetic operations."""
-    from calculator_1 import add, sub, mul, div
-    import sys
-
-    if len(sys.argv) - 1 != 3:
+    argc = len(argv[1:])
+    if argc != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-
-    op = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(op.keys()):
-        print("Unknown operator. Available operators: +, - , *, /")
-        sys.exit(1)
-
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, op[sys.argv[2]](a, b)))
+        exit(1)
+    a = int(argv[1])
+    op = argv[2]
+    b = int(argv[3])
+    if op not in ["+", "-", "*", "/"]:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    if op == "+":
+        res = a + b
+    elif op == "-":
+        res = a - b
+    elif op == "*":
+        res = a * b
+    elif op == "/":
+        res = a / b
+    print(f"{a} {op} {b} = {res}")
